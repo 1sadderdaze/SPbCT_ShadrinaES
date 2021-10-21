36a893028a6a7d9ff4e052fab37d92bf359afc40
1sadderdaze <75395908+1sadderdaze@users.noreply.github.com>
2021-10-21 22:43:29 +0300

    Delete 1primer directory

--- 1primer/1primer/1primer.cpp
+++
@@ -1,9 +0,0 @@
-﻿#include <stdlib.h>
-#include <stdio.h>
-int main() {
-	char* some_memory = (char*)0;
-	printf("A read from null %s", some_memory);
-	sprintf(some_memory, "A write to null");
-	exit(EXIT_SUCCESS);
-}
-
